11e0b54c7d573a6d28c4d3e4340b6c9ff462f3f4
leonardo barrachi <leonardobarrachi@yahoo.com.br>
2023-04-02 16:36:16 -0300

    02/03/23 Botão sair configurado na tela principal

--- app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
+++ app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
@@ -1,12 +1,25 @@
 package com.example.meuprimeiroappkt.view.principal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.meuprimeiroappkt.R
+import com.example.meuprimeiroappkt.databinding.ActivityPrincipalBinding
+import com.example.meuprimeiroappkt.view.login.Login
+import com.google.firebase.auth.FirebaseAuth
 
 class Principal : AppCompatActivity() {
+    private lateinit var binding:ActivityPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSair.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()//recupera a instancia do servidor e sai do sistema pelo .sigOut
+            val voltaTelaLogin = Intent(this, Login::class.java) // após sair volta para tela login
+            startActivity(voltaTelaLogin) // ir para tela login
+            finish() // fim
+        }
     }
 }
