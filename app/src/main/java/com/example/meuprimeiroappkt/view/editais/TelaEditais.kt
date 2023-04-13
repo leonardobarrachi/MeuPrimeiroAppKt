74ba7e4ddff6f440ad6b2da061185afd83101d79
leonardo barrachi <leonardobarrachi@yahoo.com.br>
2023-04-13 13:11:20 -0300

    13/04/23 Cong de navegação para activity_tela_editais.xml

---
+++ app/src/main/java/com/example/meuprimeiroappkt/view/editais/TelaEditais.kt
@@ -0,0 +1,16 @@
+package com.example.meuprimeiroappkt.view.editais
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meuprimeiroappkt.R
+
+class TelaEditais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_editais)
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
+++ app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meuprimeiroappkt.databinding.ActivityPrincipalBinding
+import com.example.meuprimeiroappkt.view.editais.TelaEditais
 import com.example.meuprimeiroappkt.view.login.Login
 import com.google.firebase.auth.FirebaseAuth
 
@@ -12,15 +13,24 @@ class Principal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btSair.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()//recupera a instancia do servidor e sai do sistema pelo .sigOut
+            FirebaseAuth.getInstance()
+                .signOut()//recupera a instancia do servidor e sai do sistema pelo .sigOut
             val voltaTelaLogin = Intent(this, Login::class.java) // após sair volta para tela login
             startActivity(voltaTelaLogin) // ir para tela login
             finish() // fim
-            
+
         }
+        binding.button.setOnClickListener {
+            val irEditais = Intent(this,TelaEditais::class.java)
+            startActivity(irEditais)
+        }
+
+
     }
+
 }
