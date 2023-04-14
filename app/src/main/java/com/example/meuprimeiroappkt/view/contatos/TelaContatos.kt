297178a94b7885008ce3015e9f4f16b4b324b818
leonardo barrachi <leonardobarrachi@yahoo.com.br>
2023-04-14 17:35:57 -0300

    14/04/23 Cong de navegação dos botões da activity principal finalizada

---
+++ app/src/main/java/com/example/meuprimeiroappkt/view/contatos/TelaContatos.kt
@@ -0,0 +1,12 @@
+package com.example.meuprimeiroappkt.view.contatos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meuprimeiroappkt.R
+
+class TelaContatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_contatos)
+    }
+}
--- app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
+++ app/src/main/java/com/example/meuprimeiroappkt/view/principal/Principal.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meuprimeiroappkt.databinding.ActivityPrincipalBinding
+import com.example.meuprimeiroappkt.view.contatos.TelaContatos
 import com.example.meuprimeiroappkt.view.editais.TelaEditais
 import com.example.meuprimeiroappkt.view.eventos.TelaEventos
 import com.example.meuprimeiroappkt.view.login.Login
+import com.example.meuprimeiroappkt.view.orcamento.TelaOrcamento
 import com.example.meuprimeiroappkt.view.turismo.TelaTurismo
 import com.google.firebase.auth.FirebaseAuth
 
@@ -14,34 +16,43 @@ class Principal : AppCompatActivity() {
     private lateinit var binding:ActivityPrincipalBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+            super.onCreate(savedInstanceState)
 
-        binding = ActivityPrincipalBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            binding = ActivityPrincipalBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        binding.btSair.setOnClickListener {
-            FirebaseAuth.getInstance()
-                .signOut()//recupera a instancia do servidor e sai do sistema pelo .sigOut
-            val voltaTelaLogin = Intent(this, Login::class.java) // após sair volta para tela login
-            startActivity(voltaTelaLogin) // ir para tela login
-            finish() // fim
+            binding.btSair.setOnClickListener {
+                FirebaseAuth.getInstance()
+                    .signOut()//recupera a instancia do servidor e sai do sistema pelo .sigOut
+                val voltaTelaLogin = Intent(this, Login::class.java) // após sair volta para tela login
+                startActivity(voltaTelaLogin) // ir para tela login
+                finish() // fim
 
+            }
+            binding.button.setOnClickListener {
+                val irEditais = Intent(this,TelaEditais::class.java)
+                startActivity(irEditais)
+            }
+
+            binding.button4.setOnClickListener {
+                val irEventos = Intent(this,TelaEventos::class.java)
+                startActivity(irEventos)
+            }
+
+            binding.button5.setOnClickListener {
+                val irTurismo = Intent(this,TelaTurismo::class.java)
+                startActivity(irTurismo)
+            }
+
+             binding.button6.setOnClickListener {
+               val irOrcamento = Intent(this,TelaOrcamento::class.java)
+                 startActivity(irOrcamento)
         }
-        binding.button.setOnClickListener {
-            val irEditais = Intent(this,TelaEditais::class.java)
-            startActivity(irEditais)
-        }
 
-        binding.button4.setOnClickListener {
-            val irEventos = Intent(this,TelaEventos::class.java)
-            startActivity(irEventos)
+             binding.button7.setOnClickListener {
+                 val irContatos = Intent(this,TelaContatos::class.java)
+                 startActivity(irContatos)
         }
-
-        binding.button5.setOnClickListener {
-            val irTurismo = Intent(this,TelaTurismo::class.java)
-            startActivity(irTurismo)
-        }
-
 
 
 
